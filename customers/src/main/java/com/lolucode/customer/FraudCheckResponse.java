5ce33fba4a45fbcb72572a1ff13e5ac107799ede
lolucodes <happycode20206@gmail.com>
2022-08-10 13:51:33 +0100

    fraud check microservice

---
+++ customers/src/main/java/com/lolucode/customer/FraudCheckResponse.java
@@ -0,0 +1,5 @@
+package com.lolucode.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+
+}
---
+++ fraud/src/main/java/com/lolucode/fraud/FraudCheckResponse.java
@@ -0,0 +1,5 @@
+package com.lolucode.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+
+}
